e0c04008034f28498b6ca2c93664e024b2591a97
P.Young <pyoung@witekio.com>
2024-05-06 13:14:12 +0100

    added securezone zip & submodule init

--- IoTConnect/scripts/setup-project.sh
+++ IoTConnect/scripts/setup-project.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+echo "Check on submodule."
+git -C ../../ submodule init
+git -C ../../ submodule update
+
 echo "Unzipping en.x-cube-aws-h5-v1-0-0.zip"
 unzip -q ../en.x-cube-aws-h5-v1-0-0.zip
 
+echo "Unzipping en.x-cube-sec-m-h5-v1-1-1.zip"
+unzip -q ../en.x-cube-sec-m-h5-v1-1-1.zip
+
 echo "Ensure all shell scripts are executable."
-find STM32CubeExpansion_Cloud_AWS_H5_V1.0.0/ -name "*.sh" -exec chmod +x {} \;
+find STM32CubeExpansion_Cloud_AWS_H5_V1.0.0/ \
+	X-Cube-SEC-M-H5_V1.1.1/ \
+	-name "*.sh" -exec chmod +x {} \;
 
 echo "Move & tidy of sources"
-mv STM32CubeExpansion_Cloud_AWS_H5_V1.0.0/* .
-rm -r STM32CubeExpansion_Cloud_AWS_H5_V1.0.0/
+cp -rn STM32CubeExpansion_Cloud_AWS_H5_V1.0.0/* .
+cp -rn X-Cube-SEC-M-H5_V1.1.1/* .
+
+rm -r STM32CubeExpansion_Cloud_AWS_H5_V1.0.0/ \
+	X-Cube-SEC-M-H5_V1.1.1
 
 echo "Patching sources"
 patch --strip=1 < IoTConnect/scripts/STM32CubeExpansion_Cloud_AWS_H5_V1.0.0.patch
